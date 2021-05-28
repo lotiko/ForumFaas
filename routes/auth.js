5c18a5e5c7abf4907c2fadc4d2aa6719c2e53ecb
lotiko <l.despres@netc.fr>
2021-05-28 15:16:46 +0200

    add check mail server side+ error message

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const router  = express.Router();
-
-const Users = require('../models/user')
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const Users = require("../models/user");
+var validator = require("email-validator");
 // const baseDataview = {
 //     frame: true,
 //     title: 'home'
@@ -13,37 +15,63 @@ const Users = require('../models/user')
 //     });
 // });
 
-router.get('/signUp', (req, res, next) => {
+router.get("/signUp", (req, res, next) => {
+  res.render("auth/signup");
+});
 
-    res.render('auth/signup')
-})
+router.post("/signUp", (req, res, next) => {
+  let nameAdd = req.body.pseudo;
+  let emailAdd = req.body.email;
+  let passwordAdd = req.body.password;
+  let oldValues = { pseudo: req.body.pseudo, email: req.body.email };
+  // 1. Check username, email and password are not empty
+  if (!nameAdd || !passwordAdd || !emailAdd) {
+    res.render("auth/signup", {
+      errorMessage: "Tous les champs sont obligatoire.",
+      oldValues: oldValues,
+    });
+    return;
+  }
+  // 2. check valide email regex
+  if (!validator.validate(emailAdd)) {
+    res.render("auth/signup", {
+      errorMessage: "Email non valide.",
+      oldValues: oldValues,
+    });
+    return;
+  }
+  // Encrypt the password
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(passwordAdd, salt);
 
-router.post('/signUp', (req, res, next) => {
+  const adduser = new Users({
+    name: nameAdd,
+    email: emailAdd,
+    password: hashPass,
+  });
+  adduser
+    .save()
+    .then(function (usersFromDb) {
+      res.render("home", {
+        message: "Votre compte à bien été enregistré vous pouvez désormais vous connecter.",
+      });
+    })
+    /// voir ici si message dans modéles ou ici
+    .catch((err) => {
+      if (err.code === 11000) {
+        res.render("auth/signup", { errorMessage: "Un utilisateur avec ce mail existe deja." });
+        return;
+      }
+      next(err);
+    });
+});
 
-    let nameAdd = req.body.pseudo;
-    let emailAdd = req.body.email;
-    let passwordAdd = req.body.password;
-   
-        const adduser=new Users({
-            name:nameAdd,
-            email:emailAdd,
-            password:passwordAdd
-            
-        })
-        adduser.save()
-        .then(function(usersFromDb){
-            res.redirect('/')
-        })
-        .catch((err)=>console.log(err))
-   
-})
-
-router.get('/login',(req,res,next)=>{
-    res.render('auth/login')
-})
- router.post('/login',(req,res,next)=>{
-     console.log(req.body);
-     res.render('home')
- })
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+router.post("/login", (req, res, next) => {
+  console.log(req.body);
+  res.render("home");
+});
 
 module.exports = router;
