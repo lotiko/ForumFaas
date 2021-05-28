647acc53dc38c069798d56797215b7b8e963b00e
lotiko <l.despres@netc.fr>
2021-05-28 10:23:03 +0200

    backup

--- routes/function.js
+++ routes/function.js
@@ -24,47 +24,70 @@ router
         next(err);
       });
   })
-  .post("/result/:name", (req, res, next) => {
+  .post("/result/:name", async (req, res, next) => {
     let datatoret;
     let id;
-    Fun.findOne({ name: req.params.name })
-      .then((ret) => {
-        let argsNames = [];
-        id = ret._id;
-        let path = `${__dirname}/../tmpfolder/${ret._id}.js`;
-        // let path = `/var/lib/docker/volumes/volTestNode/${ret._id}.js`;
-        let args = ret.args.map((el) => {
-          argsNames.push(el);
-          if (el.match(/^str/)) return String(`${el} = "${req.body[el]}"`);
-          return `${el} = ${req.body[el]}`;
-          // return req.body[el];
-        });
-        // console.log(args, __dirname);
-        let fileText = `
-function ${ret.name}(${args}) {
-${ret.body}
-}
-module.exports = ${ret.name};`;
+    //     Fun.findOne({ name: req.params.name })
+    //       .then((ret) => {
+    //         let argsNames = [];
+    //         id = ret._id;
+    //         let path = `${__dirname}/../tmpfolder/${ret._id}.js`;
+    //         // let path = `/var/lib/docker/volumes/volTestNode/${ret._id}.js`;
+    //         let args = ret.args.map((el) => {
+    //           argsNames.push(el);
+    //           if (el.match(/^str/)) return String(`${el} = "${req.body[el]}"`);
+    //           return `${el} = ${req.body[el]}`;
+    //           // return req.body[el];
+    //         });
+    //         // console.log(args, __dirname);
+    //         let fileText = `
+    // function ${ret.name}(${args}) {
+    // ${ret.body}
+    // }
+    // module.exports = ${ret.name};`;
 
-        fs.appendFileSync(path, fileText, function (err) {
-          if (err) throw err;
-          console.log("Saved!");
-        });
-        // const fun = require(`../tmpfolder/${ret.name}.js`);
-        // let datatoret = fun(req.body.nb, req.body.str);
-        // res.json({ data: datatoret });
-        // res.send("test")
-        // .catch((err) => console.log("in ctch==============", err));
-        datatoret = fetch("http://localhost:2323/indb/launch/" + id, {
-          headers: { "Content-type": "application/json" },
-        })
-          .then((ret) => ret.json())
-          .then((json) => {
-            console.log("in fetch================", json);
-            res.json(json);
-          });
-      })
-      .catch((err) => next(err));
+    //         fs.appendFileSync(path, fileText, function (err) {
+    //           if (err) throw err;
+    //           console.log("Saved!");
+    //         });
+    // const fun = require(`../tmpfolder/${ret.name}.js`);
+    // let datatoret = fun(req.body.nb, req.body.str);
+    // res.json({ data: datatoret });
+    // res.send("test")
+    // .catch((err) => console.log("in ctch==============", err));
+    //   datatoret = fetch("http://localhost:2323/indb/launch/" + id, {
+    //     headers: { "Content-type": "application/json" },
+    //   })
+    //     .then((ret) => ret.json())
+    //     .then((json) => {
+    //       console.log("in fetch================", json);
+    //       res.json(json);
+    //     });
+    // })
+    // .catch((err) => next(err));
+    try {
+      console.log("hey you are here");
+      datatoret = await fetch("http://localhost:2323/indb/launch/" + req.params.name, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(req.body),
+      }).then((ret) => {
+        console.log(ret);
+        return ret.json();
+      });
+      // .then((json) => {
+      //   console.log("in fetch================", json);
+      //   // res.json(json);
+      //   return json;
+      //   // console.log(datatoret);
+      //   // res.json(datatoret);
+      // });
+      console.log(datatoret);
+      res.json(datatoret);
+    } catch (error) {
+      console.log(err);
+    }
+    // res.redirect(307, "http://localhost:2323/indb/launch/" + req.params.name);
   });
 
 module.exports = router;
