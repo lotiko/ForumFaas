26393650ac94f753e33039fe8aef21468c08ae7c
lotiko <l.despres@netc.fr>
2021-05-31 18:32:04 +0200

    add errors handler for image upload

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const validator = require("email-validator");
-const passport = require("passport");
 const fileUploader = require("../configs/cloudinary.config");
+const upload = fileUploader.single("avatar");
+const multer = require("multer");
 const routeGuard = require("../configs/route-gard-isLog");
 
 router.use(routeGuard); //only authenthicate user can use this router
@@ -20,26 +21,52 @@ router
     console.log(req.session);
     res.render("user/edit", { isLog: true, title: "EditUser", ...dataView });
   })
-  .post("/edit", fileUploader.single("avatar"), (req, res, next) => {
-    // console.log(req.user.createdAt.toLocaleString());
-    const { email, name, description } = req.body;
-    const avatar = req.file ? req.file.path : req.user.avatar;
-    // const dataView = getDataView(req.user);
-    console.log(req.body, "hererereerr");
-    User.findById(req.user._id)
-      .then((userFromdb) => {
-        userFromdb.avatar = avatar;
-        userFromdb.name = name;
-        userFromdb.email = email;
-        userFromdb.descriptions = description;
-        userFromdb.save().then((updatedUser) => {
-          console.log("oldUser => ", userFromdb, "newuser => ", updatedUser);
+  .post(
+    "/edit",
+    (req, res, next) => {
+      upload(req, res, (err) => {
+        console.log(err);
+        if (err) {
+          res.render("user/edit", {
+            errorMessage: "Probléme avec le fichier image seul jpg, png, jpeg sont acceptés.",
+            ...getDataView(req.user),
+          });
+          return;
+        } else {
+          next();
+        }
+      });
+    },
+    (req, res, next) => {
+      console.log(req.user.createdAt.toLocaleString());
+      console.log(req.file);
+      const { email, name, description } = req.body;
+      const avatar = req.file ? req.file.path : req.user.avatar;
+      // check valide email regex
+      if (!validator.validate(email)) {
+        res.render("user/edit", {
+          errorMessage: "Email non valide.",
+          title: "EditUser",
         });
-        res.redirect("/user");
-      })
-      .catch((err) => next(err));
-    // res.render("user/edit", { isLog: true, title: "EditUser", ...dataView });
-  });
+        return;
+      }
+      // const dataView = getDataView(req.user);
+      console.log(req.body, "hererereerr");
+      User.findById(req.user._id)
+        .then((userFromdb) => {
+          userFromdb.avatar = avatar;
+          userFromdb.name = name;
+          userFromdb.email = email;
+          userFromdb.descriptions = description;
+          userFromdb.save().then((updatedUser) => {
+            console.log("oldUser => ", userFromdb, "newuser => ", updatedUser);
+          });
+          res.redirect("/user");
+        })
+        .catch((err) => next(err));
+      // res.render("user/edit", { isLog: true, title: "EditUser", ...dataView });
+    }
+  );
 
 // utils
 function getDataView(user) {
