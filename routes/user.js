3983cad0e41f3cf872a6d429c4d31c4911c8532b
lotiko <l.despres@netc.fr>
2021-05-31 13:59:15 +0200

    view user account data no css

--- routes/user.js
+++ routes/user.js
@@ -10,8 +10,18 @@ router.get("/", (req, res, next) => {
   if (!isLog) {
     res.redirect("/");
   }
-  console.log(req.user);
-  res.render("user/account", { isLog: isLog, title: "Account" });
+  // console.log(req.user.createdAt.toLocaleString());
+  const dataView = {
+    avatar: req.user.avatar,
+    name: req.user.name,
+    email: req.user.email,
+    description: req.user.description,
+    nbPost: req.user.posts.length,
+    nbFunction: req.user.functions.length,
+    status: req.user.status,
+    creationDate: req.user.createdAt.toLocaleString(),
+  };
+  res.render("user/account", { isLog: isLog, title: "Account", ...dataView });
 });
 
 module.exports = router;
