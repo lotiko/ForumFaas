b1646cc1e92483604924431fd03606fb16af657e
lotiko <l.despres@netc.fr>
2021-06-06 01:57:12 +0200

    presentations to home forum

--- routes/forum.js
+++ routes/forum.js
@@ -88,10 +88,78 @@ router.get("/:catname", async (req, res, next) => {
   }
   if (req.params.catname === "home") {
     try {
+      const { page = 1, limit = 4 } = req.query;
       let fun = await Function.find({}).sort({ createdAt: -1 }).exec();
       let post = await PostModel.find({ categorie: "question" }).sort({ createdAt: -1 }).exec();
+      // execute query with page and limit values
+      const users = await User.find()
+        .select("-password")
+        .limit(limit * 1)
+        .skip((page - 1) * limit)
+        .exec();
+      // compter le nombre de présentations et calculer le nombre de page en conséquences pour pagination dans vue
+      const nbDoc = await User.countDocuments({}).exec();
+      let nbPage = Math.floor(nbDoc / limit);
+      if (nbDoc % nbPage !== 0) {
+        nbPage++;
+      }
 
+      const usersDataView = users.map((el) => {
+        return {
+          ...el._doc,
+          nbPost: el.posts.length,
+          nbFun: el.functions.length,
+        };
+      });
+      // on retourne seulement la donnée si query data sinon on envoi la vue
+      if (req.query.data) {
+        res.json({
+          users: usersDataView,
+          nbPage: nbPage,
+          page: page,
+          limit: limit,
+        });
+        return;
+      }
+      // console.log(usersDataView);
+      let pagination = { one: false, two: false, tree: false, four: false, more: false };
+      if (nbPage < 3) {
+        pagination.one = true;
+        pagination.two = true;
+      } else if (nbPage < 4) {
+        pagination.one = true;
+        pagination.two = true;
+        pagination.tree = true;
+      } else {
+        if (nbPage > 4) pagination.more = true;
+        pagination.one = true;
+        pagination.two = true;
+        pagination.tree = true;
+        pagination.four = true;
+      }
+      // dev
+      pagination.more = true;
+
+      // res.render("forum/presentation", {
+      //   users: usersDataView,
+      //   nbPage: nbPage,
+      //   page: page,
+      //   limit: limit,
+      //   pagination: pagination,
+      //   title: "ForumHome",
+      //   style: "presentations",
+      //   script: "presentations",
+      //   isLog: !!req.user,
+      // });
       res.render("forum/home", {
+        users: usersDataView,
+        nbPage: nbPage,
+        page: page,
+        limit: limit,
+        pagination: pagination,
+        title: "ForumHome",
+        style: "presentations",
+        script: "presentations",
         isLog: !!req.user,
         posts: post,
         functions: fun,
@@ -148,49 +216,6 @@ router.post("/:catname", (req, res, next) => {
   }
   // /////////////ANSWER///////////////////
   if (req.params.catname === "answer") {
-    console.log(req.body, req.query);
-    const { title = "answer", body } = req.body;
-    const categorie = req.query.type;
-    let fromQuestion = false;
-    if (categorie === "answer") {
-      fromQuestion = req.query.from;
-    }
-    const userId = req.user._id;
-    // const withFunction=req.function._id
-
-    console.log(req.user._id);
-
-    console.log("title", title);
-    if (!title || !body) {
-      res.render("forum/answer", {
-        isLog: true,
-        errorMessage: "Veuillez remplir le titre",
-        style: "answer",
-        module: "answer",
-      });
-      return;
-    }
-    let newPost = {
-      title: title,
-      body: body,
-      categorie: categorie,
-      userId: userId,
-    };
-    console.log("mon nvx poste", newPost);
-    if (fromQuestion) {
-      newPost.fromQuestion = fromQuestion;
-    }
-    new PostModel(newPost)
-      .save()
-      .then(function (answersFromDb) {
-        if (categorie === "question") {
-          res.redirect("/forum/home");
-        }
-        if (categorie === "answer") {
-          res.redirect("/forum/answer/" + fromQuestion);
-        }
-      })
-      .catch((err) => next(err));
   }
   // si pas de route trouver continue vers 404 error
 });
@@ -247,6 +272,49 @@ router.get("/:catname/new", (req, res, next) => {
   if (req.params.catname === "home") {
   }
   if (req.params.catname === "answer") {
+    console.log(req.body, req.query);
+    const { title = "answer", body } = req.body;
+    const categorie = req.query.type;
+    let fromQuestion = false;
+    if (categorie === "answer") {
+      fromQuestion = req.query.from;
+    }
+    const userId = req.user._id;
+    // const withFunction=req.function._id
+
+    console.log(req.user._id);
+
+    console.log("title", title);
+    if (!title || !body) {
+      res.render("forum/answer", {
+        isLog: true,
+        errorMessage: "Veuillez remplir le titre",
+        style: "answer",
+        module: "answer",
+      });
+      return;
+    }
+    let newPost = {
+      title: title,
+      body: body,
+      categorie: categorie,
+      userId: userId,
+    };
+    console.log("mon nvx poste", newPost);
+    if (fromQuestion) {
+      newPost.fromQuestion = fromQuestion;
+    }
+    new PostModel(newPost)
+      .save()
+      .then(function (answersFromDb) {
+        if (categorie === "question") {
+          res.redirect("/forum/home");
+        }
+        if (categorie === "answer") {
+          res.redirect("/forum/answer/" + fromQuestion);
+        }
+      })
+      .catch((err) => next(err));
   }
 });
 module.exports = router;
